027b3bacca0ff29d86287e62896b99c8f8fd37e9
Hero Jay G. Calado <82526899+xzyqrn@users.noreply.github.com>
2026-08-24 19:39:15 +0800

    Add xzyqrn qr studio: 2D styles, kits, motion, 3D mode

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-PLACEHOLDER
+import QRCodeStyling, {
+  type DotType,
+  type CornerSquareType,
+  type CornerDotType,
+  type ErrorCorrectionLevel,
+  type Options,
+} from "qr-code-styling";
+import { GIFEncoder, quantize, applyPalette } from "gifenc";
+import { ART_STYLES, getStyle, type ArtStyleId } from "./styles";
+import { BRAND_KITS, type KitId } from "./kits";
+import { Qr3dViewer } from "./qr3d";
+import "./styles.css";
+
+// NOTE: truncated push - will be replaced
+export {};
